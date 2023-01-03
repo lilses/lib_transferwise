88aac0aae665b45d7ba381106270771de351f07d
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-01-03 15:32:08 +0000

    refactor: add bank statement

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 use macros_make_error::make_error2;
+use macros_make_model::make_model22;
+use serde::*;
 
 pub mod deposit;
 pub mod statement;
 
 make_error2!(TransferWiseError);
+
+make_model22!(
+    QTransferWiseSca,
+    ITransferWiseSca,
+    OTransferWiseSca,
+    transferwise_sca,
+    signature: String
+);
--- src/statement.rs
+++ src/statement.rs
@@ -1,4 +1,5 @@
-use crate::TransferWiseError;
+use crate::{transferwise_sca, ITransferWiseSca, TransferWiseError};
+use chrono::SecondsFormat;
 use macros_create_app::make_app65;
 use macros_make_model::make_model22;
 use macros_make_model::make_model23;
@@ -28,9 +29,9 @@ make_model23!(
     ITransferWiseStatementTxDetails,
     OTransferWiseStatementTxDetails,
     description: String,
-    sender_name: String,
-    sender_account: String,
-    payment_reference: String
+    sender_name: Option<String>,
+    sender_account: Option<String>,
+    payment_reference: Option<String>
 );
 
 make_model23!(
@@ -91,39 +92,116 @@ make_app65!(
 make_scope!("transferwise", [post, wise_statement]);
 
 async fn handle(
-    my_state: actix_web::web::Data<MyState>,
+    s: actix_web::web::Data<MyState>,
     json: actix_web::web::Json<ITransferWiseStatement>,
-) -> Result<(), TransferWiseError> {
-    let reqw = &my_state.req;
+) -> Result<ITransferWiseStatement, TransferWiseError> {
+    let reqw = &s.req;
 
-    let yesterday = chrono::Utc::now() - chrono::Duration::days(1);
-    let url = format!(
-        "https://api.transferwise.com/v1/profiles/{}/balance-statements/{}/statement.json?currency=GBP&intervalStart={}&intervalEnd={}&type=COMPACT",
-        my_state.env.transferwise_account_id,
-        my_state.env.transferwise_balance_id,
-        yesterday,
-        chrono::Utc::now()
-    );
-    let k = reqw
-        .get(url)
-        .bearer_auth(&my_state.env.transferwise_pat)
-        .send()
-        .await
-        .map_err(TransferWiseError::from_general)?
-        .error_for_status()
-        .map_err(TransferWiseError::from_general)?;
+    // let scas = transferwise_sca::postgres_query::get(&s.sqlx_pool)
+    //     .await
+    //     .map_err(TransferWiseError::from_general)?;
 
-    println!("{:?}", k.headers());
+    if json.transactions.is_empty() {
+        Ok(ITransferWiseStatement::default())
+    } else {
+        let tx = &json.transactions[0];
+        let payment_reference =
+            tx.details
+                .payment_reference
+                .clone()
+                .ok_or(TransferWiseError::GeneralError(
+                    "no payment referece".to_string(),
+                ))?;
 
-    Ok(())
+        let yesterday = chrono::Utc::now() - chrono::Duration::days(1);
+        let url = format!(
+            "https://api.transferwise.com/v1/profiles/{}/balance-statements/{}/statement.json?currency=GBP&intervalStart={}&intervalEnd={}&type=COMPACT",
+            s.env.transferwise_account_id,
+            s.env.transferwise_balance_id,
+            yesterday.to_rfc3339_opts(SecondsFormat::Millis, true),
+            chrono::Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true),
+        );
+
+        let k = reqw
+            .get(&url)
+            .bearer_auth(&s.env.transferwise_pat)
+            .send()
+            .await
+            .map_err(TransferWiseError::from_general)?;
+
+        if k.status()
+            == actix_web::http::StatusCode::from_u16(403)
+                .map_err(TransferWiseError::from_general)?
+        {
+            println!("sca required");
+            let headers = k.headers();
+            let code = headers.get("X-2FA-Approval").unwrap();
+
+            let rsa =
+                openssl::rsa::Rsa::private_key_from_pem(s.env.transferwise_private_pem.as_bytes())
+                    .map_err(TransferWiseError::from_general)?;
+            let keypair =
+                openssl::pkey::PKey::from_rsa(rsa).map_err(TransferWiseError::from_general)?;
+            let mut signer =
+                openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &keypair)
+                    .map_err(TransferWiseError::from_general)?;
+            signer
+                .update(code.as_bytes())
+                .map_err(TransferWiseError::from_general)?;
+            let signature = signer
+                .sign_to_vec()
+                .map_err(TransferWiseError::from_general)?;
+            let encoded_signature = base64::encode(&signature);
+
+            reqw.get(&url)
+                .bearer_auth(&s.env.transferwise_pat)
+                .header(
+                    "X-2FA-Approval",
+                    code.to_str().map_err(TransferWiseError::from_general)?,
+                )
+                .header("X-Signature", encoded_signature.as_str())
+                .send()
+                .await
+                .map_err(TransferWiseError::from_general)?
+                .error_for_status()
+                .map_err(TransferWiseError::from_general)?
+                .json::<ITransferWiseStatement>()
+                .await
+                .map_err(TransferWiseError::from_general)
+                .map(|mut x| {
+                    match x
+                        .transactions
+                        .iter()
+                        .find(|z| z.details.payment_reference == Some(payment_reference.clone()))
+                    {
+                        Some(s) => {
+                            x.transactions = sqlx::types::Json(vec![s.clone()]);
+                            x
+                        },
+                        None => ITransferWiseStatement {
+                            transactions: Default::default(),
+                        },
+                    }
+                })
+        } else {
+            Err(k
+                .error_for_status()
+                .map_err(TransferWiseError::from_general)
+                .err()
+                .unwrap())
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::statement::{handle, ITransferWiseStatement};
+    use crate::statement::{
+        handle, ITransferWiseStatement, ITransferWiseStatementTx, ITransferWiseStatementTxDetails,
+    };
     use crate::TransferWiseError;
     use dotenv;
     use my_state::get_my_state;
+    use openssl::rsa::{Padding, Rsa};
 
     #[tokio::test]
     async fn test_handle() -> Result<(), TransferWiseError> {
@@ -131,7 +209,49 @@ mod tests {
         let s = get_my_state()
             .await
             .map_err(TransferWiseError::from_general)?;
-        let i = ITransferWiseStatement::default();
-        handle(actix_web::web::Data::new(s), actix_web::web::Json(i)).await
+        let mut i = ITransferWiseStatement {
+            transactions: sqlx::types::Json(vec![ITransferWiseStatementTx {
+                date: Default::default(),
+                amount: Default::default(),
+                total_fees: Default::default(),
+                details: ITransferWiseStatementTxDetails {
+                    description: "".to_string(),
+                    sender_name: None,
+                    sender_account: None,
+                    payment_reference: Some("".to_string()),
+                },
+                reference_number: "".to_string(),
+            }]),
+        };
+        let res = handle(actix_web::web::Data::new(s), actix_web::web::Json(i))
+            .await
+            .unwrap();
+        println!("{:?}", res);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_pem() -> Result<(), TransferWiseError> {
+        dotenv::dotenv().ok();
+        let s = get_my_state()
+            .await
+            .map_err(TransferWiseError::from_general)?;
+
+        let rsa = Rsa::private_key_from_pem(s.env.transferwise_private_pem.as_bytes())
+            .map_err(TransferWiseError::from_general)?;
+        let keypair = openssl::pkey::PKey::from_rsa(rsa).unwrap();
+
+        let mut signer =
+            openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &keypair).unwrap();
+        Ok(())
+    }
+
+    #[test]
+    fn test_rsa() {
+        let rsa = Rsa::generate(2048).unwrap();
+        let private_pem = rsa.private_key_to_pem().unwrap();
+        let public_pem = rsa.public_key_to_pem().unwrap();
+        std::fs::write("public.pem", public_pem).unwrap();
+        std::fs::write("private.pem", private_pem).unwrap();
     }
 }
